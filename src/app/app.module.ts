35f6a818708da7d6d01ab229d18c4a2817600e6b
jasoncheungbcit <jayson.h.cheung@gmail.com>
2016-11-22 13:12:58 -0800

    Basic landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {SuiModule} from 'ng2-semantic-ui/ng2-semantic-ui';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule, 
+    SuiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
